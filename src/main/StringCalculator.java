0f33f7dbe34f6a6cbb901706affdd49dc7f60033
Utkarsh Gupta <utkarsh.gupta591@gmail.com>
2021-09-10 16:38:09 +0530

    3rd commit

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -3,7 +3,20 @@ package main;
 public class StringCalculator {
 
 	public int add(String numbers) {
-		return 0;
+		String n[]=numbers.split(", ");
+		int a, b, sum;
+		try
+		{
+			a=Integer.parseInt(n[0]);
+			if(n.length>1) b=Integer.parseInt(n[1]);
+			else b=0;
+		}catch(NumberFormatException nfe)
+		{
+			a=0;
+			b=0;
+		}
+			sum=a+b;
+		return sum;
 	}
 
 }
