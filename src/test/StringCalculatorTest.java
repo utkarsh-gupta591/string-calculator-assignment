1fdc06590f59e3363c4c39426eaa7c0c1a7f0132
Utkarsh Gupta <utkarsh.gupta591@gmail.com>
2021-09-10 16:17:01 +0530

    1st commit

---
+++ src/test/StringCalculatorTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.StringCalculator;
+
+public class StringCalculatorTest {
+
+	private StringCalculator stringCalculator;
+	
+	@Before
+	public void setup()
+	{
+		stringCalculator=new StringCalculator();
+	}
+	
+	@Test
+	public void testAdd()
+	{
+		Assert.assertEquals(0, stringCalculator.add(""));
+	}
+}
